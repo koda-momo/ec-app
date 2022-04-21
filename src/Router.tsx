e62f4860cedf2a55cfc8f24b5d4fe7962325e095
koda-momo <momoko.kodama@rakus-partners.co.jp>
2022-04-21 18:03:21 +0900

    マイページ作成

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,6 +10,7 @@ import CartList from "./templates/CartList";
 import OrderConfirm from "./templates/OrderConfirm";
 import OrderHistory from "./templates/OrderHistory";
 import FavoList from "./templates/FavoList";
+import MyPage from "./templates/MyPage";
 
 //Switch+exactで「100%そのURLだった場合、遷移」という設定になる
 //(/)?で囲んだ文字列はあっても無くてもマッチ(ダイナミックルートと同じ感じ)
@@ -30,7 +31,7 @@ const Router = () => {
         <Route exact path={"/order/confirm"} component={OrderConfirm} />
         <Route exact path={"/order/history"} component={OrderHistory} />
         <Route exact path={"/favo"} component={FavoList} />
-        {/* <Route exact path={"/order/complete"} component={} /> */}
+        <Route exact path={"//mypage"} component={MyPage} />
         {/* <Route exact path={"/order/complete"} component={} /> */}
       </Auth>
     </Switch>
---
+++ src/templates/MyPage.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+/**
+ * 商品詳細.
+ */
+export const MyPage = () => {
+  return (
+    <>
+      <section className="c-section-wrapin">
+        <h2 className="u-text__headline">マイページ</h2>
+      </section>
+    </>
+  );
+};
+
+export default MyPage;
